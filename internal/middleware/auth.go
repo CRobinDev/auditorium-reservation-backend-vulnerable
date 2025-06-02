f078df0a7ef3360bbd4ce7e51389500bfccec36a
nathakusuma <nathakusuma07@gmail.com>
2025-06-02 22:58:08 +0700

    fix broken access control

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -49,15 +49,14 @@ func (m *Middleware) RequireAuthenticated() fiber.Handler {
 // RequireOneOfRoles dependency: RequireAuthenticated
 func (m *Middleware) RequireOneOfRoles(roles ...enum.UserRole) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
-		//userRole := ctx.Locals("user.role").(enum.UserRole)
-		//
-		//for _, role := range roles {
-		//	if userRole == role {
-		//		return ctx.Next()
-		//	}
-		//}
-		//
-		//return errorpkg.ErrForbiddenRole
-		return ctx.Next()
+		userRole := ctx.Locals("user.role").(enum.UserRole)
+
+		for _, role := range roles {
+			if userRole == role {
+				return ctx.Next()
+			}
+		}
+
+		return errorpkg.ErrForbiddenRole
 	}
 }
